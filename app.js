ff2abf7717da894ef0a37dbccd14c14a5e5c481b
BakerShoeMaker <info@quackenworth.com>
2017-10-20 12:04:46 -0700

    added all files for working timer.

---
+++ app.js
@@ -0,0 +1,32 @@
+
+window.onload = function() {
+    console.log("hello world");
+
+    startTimer();
+
+}
+function startTimer(){
+
+    var timer = new Timer();
+    $('#chronoExample .startButton').click(function () {
+        timer.start();
+    });
+    $('#chronoExample .pauseButton').click(function () {
+        timer.pause();
+    });
+    $('#chronoExample .stopButton').click(function () {
+        timer.stop();
+    });
+    $('#chronoExample .resetButton').click(function () {
+        timer.reset();
+    });
+    timer.addEventListener('secondsUpdated', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString());
+    });
+    timer.addEventListener('started', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString());
+    });
+    timer.addEventListener('reset', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString());
+    });
+}
